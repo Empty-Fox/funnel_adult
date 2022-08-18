5e77fba354789f9aa8ff6995ef592a05934c76c8
Empty-Fox <ulyana250188@gmail.com>
2022-08-18 12:00:57 +0300

    footer

--- users/js/data.js
+++ users/js/data.js
@@ -175,7 +175,7 @@ function bot_message(){
       console.log('click_one_one2: '+click_one_one);
       if(localStorage.getItem("click_one_one") == 3){
         console.log('33333');
-        timer_bot4();     
+        // timer_bot4();     
       }   
     }, 8000);    
   }
