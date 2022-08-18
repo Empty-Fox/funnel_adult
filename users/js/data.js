4870d50052c08ae2eb0202a11ea48be97334f39b
Empty-Fox <ulyana250188@gmail.com>
2022-08-18 12:18:55 +0300

    footer

--- users/js/data.js
+++ users/js/data.js
@@ -1,4 +1,8 @@
-//////////////////time in message minutes with 0 /////////////////
+input.onfocus = function () {
+  window.scrollTo(0, 0);
+  document.body.scrollTop = 0;
+}
+/////////////////time in message minutes with 0 /////////////////
 function minutes_with_leading_zeros(today) {
   return (today.getMinutes() < 10 ? '0' : '') + today.getMinutes();
 }
